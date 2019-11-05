2e0332927e809c6d388765c1c3175d65ab76851e
Gregory Ditzler <gregory.ditzler@gmail.com>
2019-11-05 14:30:18 -0700

    updated publications with papers about to be submitted and under revision.

--- build_site.R
+++ build_site.R
@@ -2,8 +2,9 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-#setwd("/home/gditzler/Git/gditzler.github.io/")
-setwd("/Users/gditzler/Git/gditzler.github.io/")
+
+setwd("/home/gditzler/Git/gditzler.github.io/")
+#setwd("/Users/gditzler/Git/gditzler.github.io/")
 
 #render your sweet site. 
 rmarkdown::render_site()
