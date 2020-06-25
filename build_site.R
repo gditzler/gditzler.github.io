bc73284fdaa01298f9b4ba12d9140883314a30a1
Gregory Ditzler <gregory.ditzler@gmail.com>
2020-06-25 15:40:13 -0400

    uipdated publications

--- build_site.R
+++ build_site.R
@@ -3,8 +3,8 @@
 #not our site because of other projects we were 
 #working on at the time. 
 
-setwd("/home/gditzler/Git/gditzler.github.io/")
-#setwd("/Users/gditzler/Git/gditzler.github.io/")
+# setwd("/home/gditzler/Git/gditzler.github.io/")
+setwd("/Users/gditzler/Git/gditzler.github.io/")
 
 #render your site. 
 rmarkdown::render_site()
