b4a277665660b6e56db5bd829c521819de6c9398
Gregory Ditzler <gregory.ditzler@gmail.com>
2022-08-10 17:15:37 -0400

    updated website structure

--- build_site.R
+++ build_site.R
@@ -1,12 +1,9 @@
+#
+# build_site.R
+#
+
 #Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
-
-# setwd("/home/gditzler/Git/gditzler.github.io/")
 setwd("/Users/gditzler/Git/gditzler.github.io/")
-# setwd("C:\\Users\\grego\\Git\\gditzler.github.io/")
-
 
 #render your site. 
 rmarkdown::render_site()
